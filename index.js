8aaad89e9cc0c9505be7197c5ecb95e741de284a
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-02-24 18:27:46 -0500

    Y.Montoya: Create param ids

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ const express = require('express');
 const app = express();
 const port = '3005';
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hola, este es mi primer servidor');
 });
 
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy un nueva ruta');
+app.get('/productos', (_, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000,
+    },
+    {
+      name: 'Producto 2',
+      price: 2000,
+    },
+    {
+      name: 'Producto 3',
+      price: 3000,
+    },
+  ]);
 });
 
-app.get('/productos', (req, res) => {
+app.get('/productos/:id', (req, res) => {
+  const { id } = req.params;
+
   res.json({
+    id,
     name: 'Producto 1',
     price: 1000,
   });
 });
 
-app.get('/home', (req, res) => {
-  res.send('Hola, Este es un home');
+app.get('/categories/:categoryId/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId,
+  });
 });
 
 app.listen(port, () => {
