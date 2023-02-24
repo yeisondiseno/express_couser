e10f7988b726b4bfa1b6d64c7d178ce991cdac3e
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-02-24 16:02:35 -0500

    Y.Montoya: Create first server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = '3005';
+
+app.get('/', (req, res) => {
+  res.send('Hola, este es mi primer servidor');
+});
+
+app.listen(port, () => {
+  console.log('Trabaja ', port);
+});
