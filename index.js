ae753fae513530534128a8cf63194b71001318b2
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-02-24 16:07:58 -0500

    Y.Montoya: Create routes

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hola, este es mi primer servidor');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy un nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000,
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.send('Hola, Este es un home');
+});
+
 app.listen(port, () => {
   console.log('Trabaja ', port);
 });
