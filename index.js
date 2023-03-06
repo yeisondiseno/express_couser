0776220b98138efdaf21ccc0c0754c3b23b99782
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-03-06 11:38:14 -0500

    Y.Montoya: Refactor CORS configuration system

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ const {
 // data
 const app = express();
 const port = '3005';
+const whitelist = ['http://example1.com', 'http://example2.com'];
 const corsOptions = {
   // more Info: https://www.npmjs.com/package/cors#usage
   // origin: 'https://example.com', // this line is waiting for a exceptional URl
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+  origin: (origin, callback) => {
+    if (!whitelist.includes(origin)) {
+      return callback(new Error('Not allowed by CORS'));
+    }
+    return callback(null, true);
+  },
 };
 
 // Init App and can get Json info
